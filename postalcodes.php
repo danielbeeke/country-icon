5fed22b2563fc7abb784a251e6ff6553a7ad7edd
Daniel Beeke <daniel@studiofonkel.nl>
2016-08-15 21:38:49 +0200

    up

--- postalcodes.php
+++ postalcodes.php
@@ -1,10 +1,12 @@
 <?php
 
-$api_url = 'https://maps.google.com/maps/api/geocode/json?key=AIzaSyBbvfdgUmI8t8289qFA-_o5yw4har1F0g8&components=country:The%20Netherlands&address=';
+$key = $argv[1];
+
+$api_url = 'https://maps.google.com/maps/api/geocode/json?key=' . $key . '&components=country:The%20Netherlands&address=';
 
 $postal_codes_geoms = json_decode(file_get_contents('postalcodes.json'), TRUE);
 
-for ($i = 1000; $i <= 10000; $i++) {
+for ($i = 8809; $i <= 10000; $i++) {
     if (!isset($postal_codes_geoms[$i])) {
         $postal_code_info_json = file_get_contents($api_url . $i);
         $postal_code_info = json_decode($postal_code_info_json, TRUE);
@@ -14,6 +16,7 @@ for ($i = 1000; $i <= 10000; $i++) {
                 $postal_codes_geoms[$i] = $postal_code_info['results'][0]['geometry']['location'];
             }
             else {
+                print_r($postal_code_info);
                 $postal_codes_geoms_json = json_encode($postal_codes_geoms, JSON_PRETTY_PRINT);
                 file_put_contents('postalcodes.json', $postal_codes_geoms_json);
             }
